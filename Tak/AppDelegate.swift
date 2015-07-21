7760793b8a16fb4e7e22ff5d33d60442acd5f1d0
Rohan Bharadwaj <ro.theprogrammer@gmail.com>
2015-07-21 16:21:27 -0700

    Added some frameworks and bridging header

--- Tak/AppDelegate.swift
+++ Tak/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import Parse
+//import ParseFacebookUtils
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,12 +21,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        
+        
         var pageControl = UIPageControl.appearance()
         pageControl.pageIndicatorTintColor = UIColor.lightGrayColor()
         pageControl.currentPageIndicatorTintColor = UIColor.grayColor()
         pageControl.backgroundColor = UIColor.whiteColor()
         
         Parse.setApplicationId("mZNskormslX8gEY3A5f9gnPytjw2BbHZ4tdtFXSC", clientKey: "AZN6tPrNwZwgV4HAkGmF8JvlbPyZBF6TQ2QZNO3w")
+        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
         /**
         let acl = PFACL()
         acl.setPublicReadAccess(true)
@@ -32,6 +38,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(application: UIApplication,
+        openURL url: NSURL,
+        sourceApplication: String?,
+        annotation: AnyObject?) -> Bool {
+            return FBSDKApplicationDelegate.sharedInstance().application(application,
+                openURL: url,
+                sourceApplication: sourceApplication,
+                annotation: annotation)
+    }
+    
+  
     
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -48,7 +66,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
+
     }
     
     func applicationWillTerminate(application: UIApplication) {
---
+++ Tak/BridgingHeader.h
@@ -0,0 +1,11 @@
+//
+//  BridgingHeader.h
+//  Tak
+//
+//  Created by Rohan Bharadwaj on 7/21/15.
+//  Copyright (c) 2015 MakeSchool. All rights reserved.
+//
+
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
