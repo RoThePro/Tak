94bf779a8d36ef3f7bd690307969a6a55725d505
Rohan Bharadwaj <ro.theprogrammer@gmail.com>
2015-07-28 16:57:58 -0700

    Added section support (fix singular commitment/section issue)

--- Tak/FeedTableViewController.swift
+++ Tak/FeedTableViewController.swift
@@ -5,6 +5,11 @@
 //  Created by Rohan Bharadwaj on 7/6/15.
 //  Copyright (c) 2015 MakeSchool. All rights reserved.
 //
+//    func rohanssorted(integers: [Int], function: (Int, Int) -> Bool ) {
+//        for integer in integers {
+//            var booleanResult = function(integer, integer)
+//        }
+//    }
 
 import UIKit
 import Parse
@@ -14,8 +19,12 @@ class FeedTableViewController: UITableViewController{
     //Helps query database
     var commit: Commit?
     //Array to store queried info
-    var objects: [Commitment]?
+    var objects = [Commitment]()
+    var testObjects = [NSDate:[Commitment]]()
+    var days = [NSDate]()
     var currentCommitment: Commitment?
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +33,26 @@ class FeedTableViewController: UITableViewController{
         //Gets data from database
         commit?.getCommitments({(results:[AnyObject]?, error:NSError?) -> Void in
             if let commitments = results as? [Commitment] {
+                for commitment in commitments{
+                   self.testObjects[commitment["date"] as! NSDate]=[]
+                }
+                for commitment in commitments{
+                    self.testObjects[commitment["date"] as! NSDate]!.append(commitment)
+                    self.days.append(commitment["date"] as! NSDate)
+                }
+                
+                func AscendingNSDate(s1: NSDate, s2: NSDate) -> Bool {
+                    var earlier = s1.earlierDate(s2)
+                    if earlier == s1 {
+                        return true
+                    }
+                    return false
+                }
+                
+                self.days = sorted(self.days, AscendingNSDate)
+                
                 self.objects = commitments
+                //println(self.days)
                 self.tableView.reloadData()
             } else {
                 // Handle error
@@ -39,27 +67,59 @@ class FeedTableViewController: UITableViewController{
     // MARK: - Table view data source
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        if(self.days.count != 0){
+            var tempDays = Set(days)
+            //println(tempDays.count)
+            days = Array(tempDays)
+            func AscendingNSDate(s1: NSDate, s2: NSDate) -> Bool {
+                var earlier = s1.earlierDate(s2)
+                if earlier == s1 {
+                    return true
+                }
+                return false
+            }
+            
+            days = sorted(days, AscendingNSDate)
+            return tempDays.count
+        }else{
+            return 1
+        }
     }
     
-    
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if(self.objects != nil){
-            return objects!.count
-        }else{
-            return 0
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if(days.count != 0){
+            var dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            return dateFormatter.stringFromDate((days[section]))
         }
+        return nil
+    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if(days.count != 0){
+            if(testObjects[days[section]]!.count != 0){
+                return testObjects[days[section]]!.count
+            }
+        }
+        
+        return 0
+        
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! FeedTableViewCell
         
-        var object = objects![indexPath.row]
+        var object=[Commitment]()
+        if(days.count != 0){
+            //println(indexPath.section)
+            object = testObjects[days[indexPath.section]]!
+        }
+        //println(object)
+        //println(days)
         
-        cell.titleLabel.text=object["title"] as? String
-        cell.descLabel.text=object["desc"] as? String
-        var impFactor = object["impFactor"]! as! Double
+        cell.titleLabel.text=object[indexPath.row]["title"] as? String
+        cell.descLabel.text=object[indexPath.row]["desc"] as? String
+        var impFactor = object[indexPath.row]["impFactor"]! as! Double
         cell.impLabel.text=String(stringInterpolationSegment: impFactor)
         
         switch impFactor{
@@ -88,16 +148,34 @@ class FeedTableViewController: UITableViewController{
     override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]? {
         
         var editAction = UITableViewRowAction(style: UITableViewRowActionStyle.Normal, title: "Edit" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
-            self.currentCommitment = self.objects![indexPath.row]
+            self.currentCommitment = self.testObjects[self.days[indexPath.section]]![indexPath.row]
             self.performSegueWithIdentifier("editCommitment", sender: self)
         })
         
         var deleteAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Delete", handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
-            self.commit?.deleteCommitment(self.objects![indexPath.row])
+            self.commit?.deleteCommitment(self.testObjects[self.days[indexPath.section]]![indexPath.row])
             self.commit?.getCommitments({ (results:[AnyObject]?, error:NSError?) -> Void in
                 if let commitments = results as? [Commitment] {
-                    self.objects = commitments
+                    for commitment in commitments{
+                        self.testObjects[commitment["date"] as! NSDate]=[]
+                    }
+                    for commitment in commitments{
+                        self.testObjects[commitment["date"] as! NSDate]!.append(commitment)
+                        self.days.append(commitment["date"] as! NSDate)
+                    }
+                    
+                    func AscendingNSDate(s1: NSDate, s2: NSDate) -> Bool {
+                        var earlier = s1.earlierDate(s2)
+                        if earlier == s1 {
+                            return true
+                        }
+                        return false
+                    }
+                    
+                    self.days = sorted(self.days, AscendingNSDate)
+                    //tableView.deleteSections(<#sections: NSIndexSet#>, withRowAnimation: <#UITableViewRowAnimation#>)
                     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                    tableView.reloadData()
                 } else {
                     println("YOU RETARD")
                 }
@@ -109,7 +187,7 @@ class FeedTableViewController: UITableViewController{
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        currentCommitment = objects![indexPath.row]
+        currentCommitment = self.testObjects[self.days[indexPath.section]]![indexPath.row]
         self.performSegueWithIdentifier("feedViewSegue", sender: self)
     }
     
@@ -137,7 +215,23 @@ class FeedTableViewController: UITableViewController{
                 commit?.getCommitments
                 commit?.getCommitments({ (results:[AnyObject]?, error:NSError?) -> Void in
                     if let commitments = results as? [Commitment] {
-                        self.objects = commitments
+                        for commitment in commitments{
+                            self.testObjects[commitment["date"] as! NSDate]=[]
+                        }
+                        for commitment in commitments{
+                            self.testObjects[commitment["date"] as! NSDate]!.append(commitment)
+                            self.days.append(commitment["date"] as! NSDate)
+                        }
+                        
+                        func AscendingNSDate(s1: NSDate, s2: NSDate) -> Bool {
+                            var earlier = s1.earlierDate(s2)
+                            if earlier == s1 {
+                                return true
+                            }
+                            return false
+                        }
+                        
+                        self.days = sorted(self.days, AscendingNSDate)
                         self.tableView.reloadData()
                     } else {
                         println("Error")
